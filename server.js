054918309d53619bc1527033eac254e80eb9c49e
JoseC982 <josecondor02@hotmail.com>
2025-06-25 16:04:14 -0500

    Agregamos un middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-app.get("/restaurantes", (request, response) => {
+
+app.use(express.json()); // para poder recibir datos en formato JSON, funciones que se van a ejecutar antes de las funciones que coincidad con la peticion, parsea el json y transforma en objeto de js
+app.get("/restaurantes/:id/:name", (req, response) => {
+    console.log("Parametros:", req.params); // vamos a obtener los parametros de ruta
     response.json({Nombre: "Restaurante San Joser", direccion: "Ladron de guevara"});
 })
 app.get('/', function (req, res) {
